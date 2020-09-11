e8cbd592daca88fd288f61734c3d71ad751ddd5b
Marlon Jones <marlonjones234@gmail.com>
2020-09-11 01:15:21 -0500

    Initial commit

---
+++ app/src/main/java/com/pancodemakes/arcoreexperiments/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pancodemakes.arcoreexperiments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    //ARCore Permission Check - This is probably going to fucking break :)
+}
